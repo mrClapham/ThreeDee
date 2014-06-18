23ceb4ad1914aa5187b28abc6456d5784c4ab87c
Graham Clapham <graham.clapham@thomsonreuters.com>
2014-06-18 15:06:25 +0100

    The hit testing now works

--- app/js/lib/ThreeDScene.js
+++ app/js/lib/ThreeDScene.js
@@ -10,6 +10,7 @@ ThreeDScene = (function (opt_target, opt_initialiser){
             target             : null,
             width              : 100,
             height             : 100,
+
             fullscreen         : true,
             backgroundColour   : 0xcccccc,
             _aspect            : null,
@@ -20,6 +21,7 @@ ThreeDScene = (function (opt_target, opt_initialiser){
             _lightAmbient      : null,
             ambientColour      : 0xff00ff,
             _lights            :[],
+
 
             //CAMERA
             fov                : 45,       //— Camera frustum vertical field of view.
@@ -49,7 +51,13 @@ ThreeDScene = (function (opt_target, opt_initialiser){
         _setUp.apply(this, arguments)
     };
 
-    function _setUp(){
+    /* Constants */
+    _scope.HOVERED          = "hovered"
+    _scope.CLICKED          = "clicked"
+    _scope.SPRITE_CLICKED   = "sprite_clicked"
+    _scope.FRAME_EVENT      = "frameEvent"
+
+    var _setUp = function(){
         //First, have you got a config object?
         // If so apply all the properies.
         if(arguments[1] && ( typeof arguments[1] == 'object') ){
@@ -63,22 +71,23 @@ ThreeDScene = (function (opt_target, opt_initialiser){
     }
 
 
-    function _onConfigSet(){
+    var _onConfigSet = function(){
         for(var value in arguments[0]){
             //Underscore properties are not to be changed.
            if(String(value).charAt(0) != '_') this._private[value] = arguments[0][value];
         }
     }
 ////
-    function _onTargetSet(){
+   var _onTargetSet =  function (){
         _init.call(this)
     }
 /////
-    function _init(){
+    var _init = function(){
         this.getRenderer().setClearColor(this.getBackgroundColour(),.0);
 
         this._private._dispatcher = document.createElement("div")
-        this._private._frameEvent = new CustomEvent("FRAME_EVENT", { 'detail': "Helooo------FRAME-------------" });
+        this._private._frameEvent = new CustomEvent(_scope.FRAME_EVENT, { 'detail': "frameEntered" });
+        this._private._clickEvent = new CustomEvent(_scope.CLICKED, { 'detail': "clicked" });
 
         if(this._private.target) this._private.target.appendChild( this.getRenderer().domElement );
 
@@ -94,10 +103,19 @@ ThreeDScene = (function (opt_target, opt_initialiser){
             this.getRenderer().setSize(this.getWidth() , this.getHeight());
             this._private._aspect = this.getWidth() / this.getHeight()
         }
+        console.log("I N I T ", this)
+        var scope = this
+        document.addEventListener( 'mousemove', function(e){
+            scope.documentMouseMove(e)
+        }, false );
+
+        document.addEventListener( 'mousedown', function(e){
+            scope.documentMouseDown(e)
+        }, false );
 
     }
 //////
-    function _initCamera(){
+   var  _initCamera =function(){
         // Create a camera, zoom it out from the model a bit, and add it to the scene.
         this._private._camera = new THREE.PerspectiveCamera(this.getFov(), this._private._aspect, this.getNear(), this.getFar());
 
@@ -108,14 +126,14 @@ ThreeDScene = (function (opt_target, opt_initialiser){
         this._private._scene.add( this.getCamera() );
     }
 
-    function _refreshCamera(){
+    var _refreshCamera = function (){
         this._private._camera.position.x = this._private.cameraX;
         this._private._camera.position.y = this._private.cameraY;
         this._private._camera.position.z = this._private.cameraZ;
         this._private._camera.updateProjectionMatrix();
     }
 /////
-    function _initLights(){
+    var _initLights = function(){
 
         // Create a light, set its position, and add it to the scene.
         var _light = this._private._light = new THREE.PointLight(this.getLightColour());
@@ -136,33 +154,85 @@ ThreeDScene = (function (opt_target, opt_initialiser){
     /*
         Default materials - if none are set
     */
-    function _initMaterials(){
+    var _initMaterials = function(){
         this._private._materials[0] = new THREE.MeshPhongMaterial({color: 0xccff33});
     }
 
-    function _initAnimation(){
+    var _initAnimation = function(){
         if(this._private.orbit) this._private._orbitControl = new THREE.OrbitControls(this._private._camera, this._private._renderer.domElement);
 
         requestAnimationFrame(this.animate.bind(this));
     }
 
-    function _onSpriteAdded(sp){
+    var _onSpriteAdded = function(sp){
      //   console.log("SpriteAdded ")
         sp.setScene( this )
     }
 
-    function _initSprites(){
+    var  _initSprites = function(){
 
     }
 
-    function _initWindowResize(){
+    var _initWindowResize = function(){
         var _this = this
         window.addEventListener('resize', function() {
            if(_this.getFullScreen()) _onWindowResize.call(_this)
         });
     }
 
-    function _onWindowResize(){
+    var _onDocumentMouseMove = function(){
+        // console.log(this._private._sprites)
+        for(var i=0; i<this._private._sprites.length; i++){
+            _hitTest.call(this, i);
+        }
+    }
+
+    var _hitTest = function(index){
+       //    console.log("Hit +"+index)
+//        var  _projector      = new THREE.Projector()
+//        var   _raycaster     = new THREE.Raycaster()
+        this._private._vector = new THREE.Vector3( this._private._mouse.x, this._private._mouse.y, 1 );
+        this._private._projector.unprojectVector( this._private._vector, this._private._camera );
+        this._private._raycaster.set( this._private._camera.position, this._private._vector.sub( this._private._camera.position ).normalize() );
+
+        var __sprite = this._private._sprites[index]
+
+        var mesh = __sprite.getMesh()
+        var intersects = this._private._raycaster.intersectObject( mesh );
+
+        if ( intersects.length > 0 ) {
+            var intersect = intersects[ 0 ];
+            var color = new THREE.Color( 1, 0, 0 );
+            var material = new THREE.MeshPhongMaterial();
+            //material.color = color
+            material.emissive = color;
+            material.shininess = 100;
+            material.shading = THREE.SmoothShading;
+
+            mesh.material = material;
+            var object = intersect.object;
+            mesh.rotation.z -= .04;
+
+            mesh.updateMatrix();
+            __sprite.setHit(true)
+
+//            _line.geometry.applyMatrix( mesh.matrix );
+//
+//            _line.visible = true;
+
+        }else{
+            try{
+                mesh.material = __sprite.getMaterial();
+            }catch(e){
+                __sprite.setHit(false)
+            }
+
+        }
+    }
+
+
+
+    var _onWindowResize = function(){
         var WIDTH = window.innerWidth,
             HEIGHT = window.innerHeight;
         this._private._renderer.setSize(WIDTH, HEIGHT);
@@ -170,22 +240,20 @@ ThreeDScene = (function (opt_target, opt_initialiser){
         this._private._camera.updateProjectionMatrix();
     }
 
-    function callBack(e){
-        console.log(e)
-    }
+
     // Renders the scene and updates the render as needed.
+/* ENUMS */
 
 /* Methods */
     _scope.prototype = {
         animate:function(){
-
             this._private._renderer.render(this._private._scene, this._private._camera);
             //this.cube.rotation.y += 0.1;
             this._private._dispatcher.dispatchEvent(this._private._frameEvent )
             requestAnimationFrame(this.animate.bind(this));
         },
         listen:function(event, opt_callback){
-            this._private._dispatcher.addEventListener("FRAME_EVENT", opt_callback)
+            this._private._dispatcher.addEventListener(event, opt_callback)
         },
         addSprite:function(value){
             var alreadyAdded = false;
@@ -199,6 +267,26 @@ ThreeDScene = (function (opt_target, opt_initialiser){
                 _onSpriteAdded.call(this, value)
             }
         },
+        documentMouseMove: function(event, targ) {
+            event.preventDefault();
+            this._private._mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+            this._private._mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+            _onDocumentMouseMove.call(this);
+        },
+        documentMouseDown:function(){
+            console.log("Mouse Down", this.getHovered());
+            this._private._dispatcher.dispatchEvent( this._private._clickEvent )
+        },
+        getHovered:function(){
+            var _hovered = [];
+            for(var sp in this._private._sprites){
+                if(this._private._sprites[sp].getHit()){
+                    _hovered.push(this._private._sprites[sp])
+                }
+            }
+            return _hovered
+        },
+
         getTarget:function(){return this._private.target},
         setTarget:function(value){ this._private.target = value, _onTargetSet.apply(this)},
         getWidth:function(){return this._private.width},
@@ -239,7 +327,7 @@ ThreeDScene = (function (opt_target, opt_initialiser){
 var standardController = function(){
 
     if( this.getMesh() ){
-        console.log("THE CONTROLLER SPRITE IS ",this.getMesh())
+       // console.log("THE CONTROLLER SPRITE IS ",this.getMesh())
         this.getMesh().rotation.x +=.005
     }
 //   if(sprite) this.sprite = sprite;
@@ -257,13 +345,26 @@ var standardController = function(){
 ThreeDSprite = (function(modelURL, material, opt_initialiser, opt_controller){
 
     var _scope = function(modelURL, material, opt_initialiser, opt_controller){
-    this._private = {} //opt_initialiser ? opt_initialiser : {};
+    this._private = {
+        hit:false,
+        skin:null,
+        bumpMap:null,
+        data:{name:"The name of the sprite is"}
+    }
     this._private._opt_initialiser = opt_initialiser ? opt_initialiser : {};
+
+        for(var value in this._private._opt_initialiser){
+            //Underscore properties are not to be changed.
+            if(String(value).charAt(0) != '_') this._private[value] = this._private._opt_initialiser[value];
+        }
+
     this._contoller = opt_controller ? opt_controller :  standardController;
     this._private._modelURL = modelURL;
     this._private._material = material;
     this._loader  = new THREE.JSONLoader();
     var scope = this
+
+
     this._loader.load( this._private._modelURL, function(geometry){
         var imgTexture = THREE.ImageUtils.loadTexture( 'models/Map-COL.jpg' )
         imgTexture.anisotropy = 1;
@@ -289,7 +390,7 @@ ThreeDSprite = (function(modelURL, material, opt_initialiser, opt_controller){
     })
     }
     // internal business logic
-    function _onSceneSet(){
+    var _onSceneSet = function(){
         //  Due to the asynchronous way the models load this mesh may not yet be defined.
         //  If so the mesh should be added during the callback from the loader (this._loader.load... etc)
         try{
@@ -298,7 +399,11 @@ ThreeDSprite = (function(modelURL, material, opt_initialiser, opt_controller){
                 //
         }
     }
-var __count = 0
+
+    var _onHitChanged = function(){
+        console.log("I'VE BEEN HIT ", this.getData().name);
+    }
+
     /* Methods */
     _scope.prototype = {
         setScene:function(value){
@@ -310,7 +415,7 @@ var __count = 0
         },
         addToScene:function(){
             this._private.scene.getScene().add(this._private._mesh);
-            this._private.scene.listen("FRAME_EVENT", function(){
+            this._private.scene.listen(_scope.FRAME_EVENT, function(){
                 this._contoller.call(this)
                //if( this.getMesh() ) this.getMesh().rotation.x +=3
                // if( this.getController() ) this.getController.call(this);
@@ -319,6 +424,27 @@ var __count = 0
         },
         getMesh:function(){
             return this._private._mesh
+        },
+        setMaterial:function(value){
+            this._private._material = value;
+        },
+        getMaterial:function(){
+            return this._private._material;
+        },
+        getHit:function(){
+            return this._private.hit
+        },
+        setHit:function(value){
+            if(this._private.hit != value){
+                this._private.hit = value;
+                _onHitChanged.call(this)
+            }
+        },
+        getData:function(){
+          return   this._private.data;
+        },
+        setData:function(value){
+            this._private.data = value;
         }
     }
 
