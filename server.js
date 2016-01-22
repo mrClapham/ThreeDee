f2fed9e6889ba565aca2eef89a3666142db9aa54
mrClapham <info@gcdesign.co.uk>
2016-01-22 11:23:51 +0000

    Develop branch created.

--- server.js
+++ server.js
@@ -1,56 +1,20 @@
-//var express = require('express');
-//var app = express();
-//
-//var config = {
-//    serverPort: 9090,
-//    directory: __dirname
-//};
-//
-//app.set('title','Flickr test');
-//
-//app.use(function(req, res, next){
-//    //console.log('%s %s', req.method, req.url);
-//    next();
-//});
-//
-//app.use(express.compress());
-//app.use(express.methodOverride());
-//app.use(express.bodyParser());
-//app.use(express.directory(config.directory))
-//app.use(express.static(config.directory));
-//
-//app.listen(config.serverPort);
-//
-//console.log('server listening at %s', config.serverPort);
+var   express = require('express')
+    , http = require('http')
+    , path = require('path');
 
-var express = require('express');
 var app = express();
 
+app.set('title','ThreeDee');
+app.use(express.static(path.join(__dirname, 'app')));
 
-var config = {
-    serverPort: 5004,
-    directory: __dirname
-};
-
-app.set('title','THREE test');
-//app.use("/media", express.static(__dirname + '/media'));
-app.use("/", express.static(__dirname, '/app'));
-//app.use("/js", express.static(__dirname + '/js'));
-
-
-/* serves main page */
-app.get("/", function(req, res) {
-    res.sendfile('index.html')
+/* serves main page - run webpack to create the files in ./dist */
+app.get('/', function (req, res) {
+    res.sendFile("monkeyShader.html", {"root": __dirname});
 });
 
+/* process.env.PORT is used in case you want to push to Heroku, for example, here the port will be dynamically allocated */
+var port = process.env.PORT || 5050;
 
-app.listen(config.serverPort);
-
-//var http = require("http");
-//http.createServer(function (request, response) {
-//    response.writeHead(200, {
-//        'Content-Type': 'text/plain'
-//    });
-//    response.write('Simple Simple Fun')
-//    response.end();
-//}).listen(5002);
+http.createServer(app).listen(port, function(){
+    console.log('Express server listening on port ' + port);
+});
